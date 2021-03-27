2baf594cc511603400ba7f2cacad045e209c4175
Amina Haq <haq.3@wright.edu>
2021-03-26 20:42:21 -0400

    Update test.cpp

--- Project3/test.cpp
+++ Project3/test.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 using namespace CryptoPP;
 
-
+//AES ENCODE GIVEN
 string aes_encode(string& plain, byte key[])
 {
 	string cipher;
@@ -28,6 +28,7 @@ string aes_encode(string& plain, byte key[])
 	return cipher;
 }
 
+//AES DECODE GIVEN
 string aes_decode(string& cipher, byte key[])
 {
 	string plain;
@@ -41,11 +42,15 @@ string aes_decode(string& cipher, byte key[])
 	return plain;
 }
 
+//Calculate the percentage of the recovered text alphabets that are in valid english
 bool percentageCalc(string plain) {
+	//if the recovered plaintext is empty return false
 	if (plain.length() == 0) {
 		return false;
 	}
 	int percent = 0;
+	//for each character in the recovered plaintext
+	//if the character is valid english, increment the precentage
 	for (int i = 0; i < plain.size(); i++) {
 		int x = plain.at(i);
 		if ((x >= 48 && x <= 59) || (x >= 44 && x <= 46) ||
@@ -55,6 +60,7 @@ bool percentageCalc(string plain) {
 		}
 	}
 	percent = (percent / (plain.length())) * 100;
+	//if the percentage of valid characters is greater than 90, return true
 	if (percent >= 90) {
 		return true;
 	}
@@ -63,35 +69,43 @@ bool percentageCalc(string plain) {
 
 int main()
 {
-	string plaintext = "Hello";
-	string ciphertext = "";
-	string recoveredtext = "";
-	byte givenkey[AES::DEFAULT_KEYLENGTH + 1] = "aaaax7qfkp3mbv9w"; //try it with "000ax7qfkp3mbv9w" and it takes a split second, anything relatively larger takes near infinite time 
-	byte guessedkey[AES::DEFAULT_KEYLENGTH + 1] = "0000x7qfkp3mbv9w";
-	string guessKeypref = "wxyz";
-	string guessKeysuff = "x7qfkp3mbv9w";
-	string guessKey = "";
-	string candidate = "";
-	string combinations = "0123456789abcdefghijklmnopqrstuvwxyz";
-	bool end = false;
-	ciphertext = ciphertext + aes_encode(plaintext, givenkey);
+	//----------START OF VARIABLE DECLARATION-------------
+	string plaintext = "Hello"; //simple plaintext that I am using for testing purpose
+	string ciphertext = ""; //variable to hold the ciphertext produced after encrypting above plaintext
+	string recoveredtext = ""; //variable to hold the text recovered for each brute force iteration
+	byte givenkey[AES::DEFAULT_KEYLENGTH + 1] = "000ax7qfkp3mbv9w"; //the key I will use
+	////try it with "000ax7qfkp3mbv9w" and it takes a split second, anything relatively larger takes near infinite time
+
+	byte guessedkey[AES::DEFAULT_KEYLENGTH + 1] = "0000x7qfkp3mbv9w"; //the key that will be brute force searched
+	string guessKeypref = "wxyz"; //the first four characters of the key
+	string guessKeysuff = "x7qfkp3mbv9w"; //the part of the key that is given
+	string candidate = ""; //this will hold the key that is identified as the correct one
+	string combinations = "0123456789abcdefghijklmnopqrstuvwxyz"; //all possible 36 combinations for the key characters
+	bool end = false; //this keeps track of whether the desired key is found or not
+
+	//---------DECRYPTING-------------
+	ciphertext = ciphertext + aes_encode(plaintext, givenkey); //
 	cout << ciphertext << endl;
-	for (int i = 0; i < 36 && !end; i++) {
+
+	//------START OF BRUTEFORCE----------
+	for (int i = 0; i < 36 && !end; i++) { //this for loop works using the combinations string, so 0000.....zzzz
 		for (int j = 0; j < 36 && !end; j++) {
 			for (int k = 0; k < 36 && !end; k++) {
 				for (int l = 0; l < 36 && !end; l++) {
-					guessKeypref[0] = combinations[i];
+					//update the prefix
+					guessKeypref[0] = combinations[i]; 
 					guessKeypref[1] = combinations[j];
 					guessKeypref[2] = combinations[k];
 					guessKeypref[3] = combinations[l];
+					//update the first four bytes of the key that will be used for bruteforce decryption
 					for (int i = 0; i < 4; i++) {
 						guessedkey[i] = guessKeypref[i];
 					}
+					//store the recovered text in a variable
 					recoveredtext = aes_decode(ciphertext, guessedkey);
+					//if the percentage is more than 90, output the key and the recovered text
 					if (percentageCalc(recoveredtext)) {
 						candidate = guessKeypref + guessKeysuff;
-					}
-					if (candidate.length() > 1) {
 						cout << "Key: " << candidate << endl;
 						cout << "Recovered text: " << recoveredtext << endl;
 						end = true;
